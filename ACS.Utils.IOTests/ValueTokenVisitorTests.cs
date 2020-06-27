85d0390bc1c4d0bfa29144518df3238f031dd453
Alain Chardonnens <alain.chardonnens@outlook.com>
2020-06-27 19:48:52 +0200

    Add ValueToken visitor tests

---
+++ ACS.Utils.IOTests/ValueTokenVisitorTests.cs
@@ -0,0 +1,30 @@
+﻿using ACS.Utils.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACS.Utils.IOTests
+{
+    [TestClass]
+    public abstract class ValueTokenVisitorTests<T> : BaseTokenVisitorTests<ValueToken<T>, TestValueToken<T>>
+    {
+        protected override TestValueToken<T> MakeTestToken() => new TestValueToken<T>(GetTestTokenValue());
+
+        protected abstract T GetTestTokenValue();
+    }
+
+    public class TestValueToken<T> : ValueToken<T>, TestTokenFunctionality
+    {
+        public bool AcceptCalled { get; private set; }
+
+        public TestValueToken(T value)
+            : base(value)
+        {
+            this.AcceptCalled = false;
+        }
+
+        protected override void DoAccept(TokenVisitor visitor)
+        {
+            this.AcceptCalled = true;
+            base.DoAccept(visitor);
+        }
+    }
+}
