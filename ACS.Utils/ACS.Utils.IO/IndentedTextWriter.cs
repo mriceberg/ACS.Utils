c35a22d3535fcd68b8e575259bf1f9c3aaa664d9
Alain Chardonnens <alain.chardonnens@outlook.com>
2022-02-13 15:04:24 +0100

    Basic IndentWriter

---
+++ ACS.Utils/ACS.Utils.IO/IndentedTextWriter.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace ACS.Utils.IO
+{
+    public class IndentedTextWriter : TextWriter
+    {
+        private readonly TextWriter _writer;
+
+        private int indentCount;
+        private string indentText;
+        private bool isFirstOnLine = true;
+
+        public IndentedTextWriter(TextWriter writer, string indentText = "\t", int indentCount = 0)
+        {
+            if (String.IsNullOrEmpty(indentText)) throw new ArgumentNullException(nameof(indentText));
+
+            this._writer = writer;
+            this.indentCount = indentCount;
+            this.indentText = indentText;
+        }
+
+        public void IncreaseIndent() => indentCount++;
+        public void DecreaseIndent()
+        {
+            if (indentCount == 0)
+            {
+                throw new InvalidOperationException("Indent is already 0.");
+            }
+
+            indentCount--;
+        }
+        public override Encoding Encoding => _writer.Encoding;
+        public override void Write(char value)
+        {
+            if (((int)value == 13) || ((int)value == 10))
+            { 
+                isFirstOnLine = true;
+            }
+            else
+            {
+                if (isFirstOnLine)
+                {
+                    for (int i = 0; i < indentCount; i++)
+                    {
+                        foreach(var c in indentText)
+                        {
+                            _writer.Write(c);
+                        }
+                    }
+                    isFirstOnLine = false;
+                }
+            }
+
+            _writer.Write(value);
+        }
+    }
+}
---
+++ ACS.Utils/ACS.Utils.IOTests/IndentedTextWriterTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACS.Utils.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ACS.Utils.IO.Tests
+{
+    [TestClass()]
+    public class IndentedTextWriterTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_EmptyIndentText_Throws()
+        {
+            using (var writer = new StringWriter())
+            {
+                new IndentedTextWriter(writer, String.Empty, 0);
+            }
+        }
+        [TestMethod]
+        public void Constructor_ExplicitIndentText_Used()
+        {
+            using (var writer = new StringWriter())
+            {
+                var actualWriter = new IndentedTextWriter(writer, "aaa", 0);
+                actualWriter.IncreaseIndent();
+                actualWriter.Write("Hello");
+                var actualText = writer.ToString();
+
+                Assert.AreEqual("aaaHello", actualText);
+            }
+        }
+        [TestMethod]
+        public void Constructor_ExplicitIndentCount_Used()
+        {
+            using (var writer = new StringWriter())
+            {
+                var actualWriter = new IndentedTextWriter(writer, "aaa", 1);
+                actualWriter.Write("Hello");
+                var actualText = writer.ToString();
+
+                Assert.AreEqual("aaaHello", actualText);
+            }
+        }
+    }
+}
